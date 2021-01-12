88b1f50b8cf3ac9f031144a973c1a2a54fb6dd15
frantisek <franta.hruska2013@gmail.com>
2021-01-13 00:20:26 +0100

    Written a library for reading keyboard matrix
    created MIDI sender function prototype

---
+++ src/keyreader.h
@@ -0,0 +1,17 @@
+#include <Arduino.h>
+
+#define COLUMNS 5
+
+struct Keyboard{
+    uint8_t size = 0x00; //0xROWS COLUMNS 
+    uint8_t output[COLUMNS];
+};
+
+void readKeyboard(struct Keyboard* _keyboard){
+
+    for (uint8_t i = 0; i < (_keyboard->size & 0x0f); i++){
+        PORTA = 1 << i;
+        _keyboard->output[i] = PINC;
+        
+    }
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,102 @@
 #include <Arduino.h>
 #include <Keypad.h>
 #include <MIDI.h>
+#include "keyreader.h"
+#include "uart.h"
 
-const uint8_t ROWS = 2;
-const uint8_t COLUMNS = 2;
+uint8_t keymap[0xff];
 
-char keymap[ROWS][COLUMNS] = {{'A', 'B'}, {'C', 'D'}};
+void sendMIDI(uint8_t status, uint8_t key, uint8_t velocity = 127){
+  switch (status){
+    case 1:
+      uart0_putc(0x9c);
+      break;
+    case 0:
+      uart0_putc(0x8c);
+      break;
+    default:
+      return;
+  }
+
+}
+
+
+char keymap[ROWS][COLUMNS];
 uint8_t row_pins[ROWS] = {25, 24};
 uint8_t col_pins[COLUMNS] = {53, 52};
 
 Keypad keyboard_matrix = Keypad(makeKeymap(keymap), row_pins, col_pins, ROWS, COLUMNS);
 
-MIDI_CREATE_DEFAULT_INSTANCE();
+struct MySettings : public midi::DefaultSerialSettings
+{
+  static const long BaudRate = 9600;
+};
+
+MIDI_CREATE_CUSTOM_INSTANCE(HardwareSerial, Serial, MIDI, MySettings);
+
+const uint8_t OCTAVE = 12;
+uint8_t TRANSPOSE = 0;
+
+void sendNotes()
+{
+  for (int i = 0; i < LIST_MAX; i++)
+  {
+    if (keyboard_matrix.key[i].stateChanged)
+    {
+      switch (keyboard_matrix.key[i].kstate)
+      {
+      case PRESSED:
+        switch (keyboard_matrix.key[i].kchar)
+        {
+        case 'A':
+          MIDI.sendNoteOn(60 , 127, 1);
+          break;
+        case 'B':
+          MIDI.sendNoteOn(61, 127, 1);
+          break;
+        case 'C':
+          MIDI.sendNoteOn(62, 127, 1);
+          break;
+        case 'D':
+          MIDI.sendNoteOn(63, 127, 1);
+          break;
+        default:
+          break;
+        }
+        break;
+      case RELEASED:
+        switch (keyboard_matrix.key[i].kchar)
+        {
+        case 'A':
+          MIDI.sendNoteOff(60 , 0, 1);
+          break;
+        case 'B':
+          MIDI.sendNoteOff(61, 0, 1);
+          break;
+        case 'C':
+          MIDI.sendNoteOff(62, 0, 1);
+          break;
+        case 'D':
+          MIDI.sendNoteOff(63, 0, 1);
+          break;
+        default:
+          break;
+        }
+        break;
+      case IDLE:
+      case HOLD:
+        break;
+      }
+    }
+  }
+}
+
+
 
 void setup()
 {
-  Serial.begin(9600);
-  // MIDI.begin(1);
+
+  uart0_init(31250);
 }
 
 void loop()
@@ -25,56 +105,7 @@ void loop()
   // fills 'key' array with keystates
   if (keyboard_matrix.getKeys())
   {
-
-    for (int i = 0; i < LIST_MAX; i++)
-    {
-      if (keyboard_matrix.key[i].stateChanged)
-      {
-        switch (keyboard_matrix.key[i].kstate)
-        {
-        case PRESSED:
-          switch (keyboard_matrix.key[i].kchar)
-          {
-          case 'A':
-            MIDI.sendNoteOn(60, 127, 1);
-            break;
-          case 'B':
-            MIDI.sendNoteOn(61, 127, 1);
-            break;
-          case 'C':
-            MIDI.sendNoteOn(62, 127, 1);
-            break;
-          case 'D':
-            MIDI.sendNoteOn(63, 127, 1);
-            break;
-          default:
-            break;
-          }
-          break;
-        case RELEASED:
-          switch (keyboard_matrix.key[i].kchar)
-          {
-          case 'A':
-            MIDI.sendNoteOff(60, 0, 1);
-            break;
-          case 'B':
-            MIDI.sendNoteOff(61, 0, 1);
-            break;
-          case 'C':
-            MIDI.sendNoteOff(62, 0, 1);
-            break;
-          case 'D':
-            MIDI.sendNoteOff(63, 0, 1);
-            break;
-          default:
-            break;
-          }
-          break;
-        case IDLE:
-        case HOLD:
-          break;
-        }
-      }
-    }
+    sendNotes();
   }
+
 }
